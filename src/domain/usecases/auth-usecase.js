1a274e38ac33ad19f1ba2ce7dfdbff82972891f0
Bruno Bandelli <brunobandelli@gmail.com>
2024-03-10 19:05:38 -0300

    auth-usecase #12 3/4

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,8 +1,12 @@
-const { MissingParamError, InvalidParamError } = require('../../utils/errors')
+const { MissingParamError,
+  //  InvalidParamError
+   } = require('../../utils/errors')
 
 module.exports = class AuthUseCase {
-  constructor (loadUserByEmailRepository) {
+  constructor (loadUserByEmailRepository, encrypter, tokenGenerator) {
     this.loadUserByEmailRepository = loadUserByEmailRepository
+    this.encrypter = encrypter
+    this.tokenGenerator = tokenGenerator
   }
 
   async auth (email, password) {
@@ -12,15 +16,19 @@ module.exports = class AuthUseCase {
     if (!password) {
       throw new MissingParamError('password')
     }
-    if (!this.loadUserByEmailRepository) {
-      throw new MissingParamError('loadUserByEmailRepository')
-    }
-    if (!this.loadUserByEmailRepository.load) {
-      throw new InvalidParamError('loadUserByEmailRepository')
-    }
+    // if (!this.loadUserByEmailRepository) {
+    //   throw new MissingParamError('loadUserByEmailRepository')
+    // }
+    // if (!this.loadUserByEmailRepository.load) {
+    //   throw new InvalidParamError('loadUserByEmailRepository')
+    // }
     const user = await this.loadUserByEmailRepository.load(email)
-    if (!user) {
-      return null
+    const isValid = user && await this.encrypter.compare(password, user.password)
+    if (isValid) {
+      const accessToken = await this.tokenGenerator.generate(user.id)
+      return accessToken
     }
+    return null
+
   }
 }
